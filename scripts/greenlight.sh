cbd814a79192b059dbe8613370d7969f5ee05c5e
Ryan van Zeben <ryan.vanzeben@prodigygame.com>
2024-07-30 20:22:29 -0400

    Add in summary panel output

--- scripts/greenlight.sh
+++ scripts/greenlight.sh
@@ -15,9 +15,13 @@ makeRequest() {
     response=$(curl -s -X GET -H "Authorization: Bearer $INPUT_API_KEY" \
      "https://app.qawolf.com/api/v0/ci-greenlight/$INPUT_ROOT_RUN_ID")
 
+    echo "### QAWolf Greenlight :wolf:" >> $GITHUB_STEP_SUMMARY
+    
     runStage=$(echo $response | jq -r '.runStage')
     if [ $runStage != "completed" ]; then
         if [ $runStage = "cancelled" ]; then
+            echo "#### Run $INPUT_ROOT_RUN_ID: CANCELLED :heavy_multiplication_x: " >> $GITHUB_STEP_SUMMARY
+
             echo "status=cancelled" >> $GITHUB_OUTPUT
             echo "Greenlight cancelled for run $INPUT_ROOT_RUN_ID"
             exit 0
@@ -26,6 +30,8 @@ makeRequest() {
         echo "Run stage is currently '$runStage', waiting for it to be 'completed' ($totalTimespan seconds elapsed)"
         numRequestsMade=$(($numRequestsMade+1))
         if [ "$numRequestsMade" -gt "$timeout" ]; then
+            echo "#### Run $INPUT_ROOT_RUN_ID: TIMED OUT :x: " >> $GITHUB_STEP_SUMMARY
+
             echo "status=timeout" >> $GITHUB_OUTPUT
             echo "Timeout reached"
             exit 1
@@ -40,18 +46,29 @@ makeRequest() {
     nonBlockingBugsCount=$(echo $response | jq -r '.nonBlockingBugsCount')
     numWorkflowRuns=$(echo $response | jq -r '.numWorkflowRuns')
     bugs=$(echo $response | jq -r '.bugs')
-    
-    echo "blockingBugsCount=$blockingBugsCount" >> $GITHUB_OUTPUT
-    echo "nonBlockingBugsCount=$nonBlockingBugsCount" >> $GITHUB_OUTPUT
-    echo "numWorkflowRuns=$numWorkflowRuns" >> $GITHUB_OUTPUT
-    echo "bugs=$bugs" >> $GITHUB_OUTPUT
-    
+
     if [ "$success" = "true" ]; then
+        echo "#### Run $INPUT_ROOT_RUN_ID: SUCCESS :white_check_mark: " >> $GITHUB_STEP_SUMMARY
         echo "status=success" >> $GITHUB_OUTPUT
+
         echo "Greenlight passed for run $INPUT_ROOT_RUN_ID"
         exit 0
     else
+        echo "#### Run $INPUT_ROOT_RUN_ID: FAILURE :x: " >> $GITHUB_STEP_SUMMARY
+        echo "---" >> $GITHUB_STEP_SUMMARY
+        echo "- $blockingBugsCount Blocking Bugs" >> $GITHUB_STEP_SUMMARY
+        echo "- $nonBlockingBugsCount Non-Blocking Bugs" >> $GITHUB_STEP_SUMMARY
+        echo "" >> $GITHUB_STEP_SUMMARY
+        echo "\`\`\`json" >> $GITHUB_STEP_SUMMARY
+        echo "$bugs" >> $GITHUB_STEP_SUMMARY
+        echo "\`\`\`" >> $GITHUB_STEP_SUMMARY
+
         echo "status=failure" >> $GITHUB_OUTPUT
+        echo "blockingBugsCount=$blockingBugsCount" >> $GITHUB_OUTPUT
+        echo "nonBlockingBugsCount=$nonBlockingBugsCount" >> $GITHUB_OUTPUT
+        echo "numWorkflowRuns=$numWorkflowRuns" >> $GITHUB_OUTPUT
+        echo "bugs=$bugs" >> $GITHUB_OUTPUT
+
         echo "Greenlight failed for run $INPUT_ROOT_RUN_ID"
         exit 1
     fi
